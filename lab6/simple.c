570510b963355a86a44dc14f8f99237e8bd749f8
Tuan Hai <nth23798@gmail.com>
2020-06-13 23:28:29 +0700

    added lab 6 sync code

---
+++ lab6/simple.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
+int shared = 10;
+void *fun(void *args)
+{
+    time_t start = time(NULL);
+    time_t end = start + 5; //run for 5 seconds
+
+    while (time(NULL) <= end)
+    {
+        shared++;
+        sleep(1);
+    }
+    
+}
+int main()
+{
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, fun, NULL);
+    pthread_join(thread_id, NULL);
+    printf("shared: %d\n", shared);
+    return 0;
+}
